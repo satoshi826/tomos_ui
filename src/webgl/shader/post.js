5cea277bb126aa95c78724f88d3a7c654e77a1a1
satoshi hata <stosto826@gmail.com>
2023-08-11 14:46:28 +0900

    manylights

--- src/webgl/shader/post.js
+++ src/webgl/shader/post.js
@@ -17,22 +17,14 @@ export const post = () => ({
 
     out vec4 outColor;
 
-    float log10(float x){
-      return log2(x) / log2(10.0);
-    }
-
     void main(void){
-
-      float aspect = resolution.x / resolution.y;
-      vec2 a = (1.0 < aspect) ? vec2(aspect, 1.0) : vec2(1.0, 1.0 / aspect);
-
       float scale = cameraPosition.z;
-
       vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
       vec2 currentP = (scale * .5 * p) + cameraPosition.xy;
-
-      float scaleLog = log10(scale);
-      float point = .1 / (scaleLog * length(postPos - currentP));
+      float point = .05 / (length(postPos - currentP));
+      if (point < .004) {
+        discard;
+      }
       outColor = vec4(vec3(point), 1.);
     }`
 
