44a24a275483b79b8cc7b7e0c660ec8249abc06b
satoshi hata <stosto826@gmail.com>
2023-08-16 23:52:25 +0900

    manylights

--- src/webgl/shader/post2.js
+++ src/webgl/shader/post2.js
@@ -15,7 +15,7 @@ export const post = () => ({
     uniform   vec2  resolution;
     uniform   vec3  cameraPosition;
     uniform   vec2  postPos;
-    out vec2 u_textureCoord;
+    out vec2 o_textureCoord;
 
     const float SCALE = .5;
 
@@ -23,7 +23,7 @@ export const post = () => ({
       float zoom = cameraPosition.z/2.;
       float aspect = resolution.y / resolution.x;
       vec2 a = (1.0 < aspect) ? vec2(1.0, 1.0 / aspect) : vec2(aspect, 1.0);
-      u_textureCoord = a_textureCoord;
+      o_textureCoord = a_textureCoord;
       gl_Position = vec4(a*(SCALE*a_position.xy - cameraPosition.xy + postPos)/zoom, 1.0, 1.0);
     }
   `,
@@ -31,12 +31,12 @@ export const post = () => ({
   frag: /* glsl */`#version 300 es
     precision highp float;
 
-    in vec2 u_textureCoord;
+    in vec2 o_textureCoord;
     out vec4 outColor;
 
     void main(void){
       vec2 center = vec2(0.5, 0.5);
-      vec2 p = u_textureCoord;
+      vec2 p = o_textureCoord;
       float point = .0005 / pow((length(center - p)),4.);
       outColor = vec4(vec3(point), 1.);
     }`
