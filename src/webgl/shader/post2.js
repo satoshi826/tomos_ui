58ae9ef1fb6119ffbc41ae994c4a141540872b84
satoshi hata <stosto826@gmail.com>
2023-08-18 23:22:46 +0900

    manyLight

--- src/webgl/shader/post2.js
+++ src/webgl/shader/post2.js
@@ -20,7 +20,7 @@ export const post = () => ({
     uniform   vec3  cameraPosition;
     out vec2 o_textureCoord;
 
-    const float SCALE = 100.;
+    const float SCALE = 10.;
 
     void main(void){
       float zoom = cameraPosition.z/2.;
@@ -41,7 +41,7 @@ export const post = () => ({
     void main(void){
       vec2 center = vec2(0.5, 0.5);
       vec2 p = o_textureCoord;
-      float point = .0002 / (length(center - p));
+      float point = .002 / (length(center - p));
       outColor = vec4(vec3(point), 1.);
     }`
 
