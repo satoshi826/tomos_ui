d3b20390b35bb5d239dc4d9f1bbb9067acd3e7e4
satoshi hata <stosto826@gmail.com>
2023-10-03 23:37:15 +0900

    infra

--- src/core/index.js
+++ src/core/index.js
@@ -156,7 +156,7 @@ export function core() {
     const [xxx, yyy] = getCurrentArea()
     infra4().user.getByLocate({xxx, yyy}).then(async(res) => {
       const p2p = res
-        .filter(u => u.id > id && u.update + 5 * 60 * 1000 > Date.now())
+        .filter(u => u.id < id && u.update + 5 * 60 * 1000 > Date.now())
         .map(({offerSDP, id}) => ({
           offerSDP, id
         }))
