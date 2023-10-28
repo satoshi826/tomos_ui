da07804b3f150b0ab4694562e53faa077814b69d
satoshi hata <stosto826@gmail.com>
2023-10-28 23:35:08 +0900

    infra

---
+++ src/core/user.js
@@ -0,0 +1,51 @@
+import {infra4} from '../infra'
+import {state} from '../../lib/state'
+import {getCurrentArea, getCamera} from '.'
+import {aToO, isEmptyO} from '../../lib/util'
+
+export const [watchUsers, setUsers, getUsers] = state({key: 'worldUsers', init: {}})
+export const [watchAddUsers, setAddUsers, getAddUsers] = state({key: 'worldUsersAdd', init: {}})
+export const [watchDelUsers, setDelUsers, getDelUsers] = state({key: 'worldUsersDel', init: {}})
+
+export const addUser = (posts) => {
+  console.log()
+  setUsers(pre => ({...pre, ...posts}))
+  setAddUsers(posts)
+}
+
+export const delUser = (keys) => {
+  setUsers(pre => {
+    keys.forEach(key => {
+      if(pre[key]) delete pre[key]
+    })
+    return pre
+  })
+  setDelUsers(keys)
+}
+
+export const user = () => {
+
+  setInterval(async() => {
+    const [xxx, yyy] = getCurrentArea()
+    infra4({ttl: 1, diff: true}).user.getByLocate({xxx, yyy}).then(users => {
+      const usersObj = aToO(users, ({id, ...v}) => [id, v])
+      !isEmptyO(usersObj) && addUser(usersObj)
+    })
+
+    const [x, y] = getCamera()
+    const id = await infra4({getLocal: true}).user.uid()
+    id && infra4({throttle: 1}).user.setLocate({
+      id,
+      x,
+      y
+    })
+  }, 1000)
+
+  watchUsers(users => {
+    console.log(users)
+  })
+
+}
+
+//----------------------------------------------------------------
+
